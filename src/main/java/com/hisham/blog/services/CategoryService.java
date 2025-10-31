0b499ffc6bedc32a01ff36ed0f41d6783049c350
Hisham Mohammed <hishammohammed1199@gmail.com>
2025-10-31 21:38:17 +0300

    add listCategories, service function to list categories

---
+++ src/main/java/com/hisham/blog/services/CategoryService.java
@@ -0,0 +1,10 @@
+package com.hisham.blog.services;
+
+import com.hisham.blog.domain.entities.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+    List<Category> listCategories();
+
+}
