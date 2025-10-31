aaff41b99e36e573b5ed88eb860a8735ad3356fa
Hisham Mohammed <hishammohammed1199@gmail.com>
2025-10-31 21:30:39 +0300

    add toDto mapper to map from Category to CategoryDto

---
+++ src/main/java/com/hisham/blog/mappers/CategoryMapper.java
@@ -0,0 +1,28 @@
+package com.hisham.blog.mappers;
+
+import com.hisham.blog.domain.dtos.CategoryDto;
+import com.hisham.blog.domain.entities.Category;
+import com.hisham.blog.domain.entities.Post;
+import com.hisham.blog.domain.entities.PostStatus;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", unmappedSourcePolicy = ReportingPolicy.IGNORE)
+public interface CategoryMapper {
+
+    @Mapping(target="postCount",source = "posts", qualifiedByName = "calculatePostCount")
+    CategoryDto toDto (Category category);
+
+    @Named("calculatePostCount")
+    default long calculatePostCount(List<Post> posts){
+        if(posts == null){
+            return 0;
+        }
+        return posts.stream()
+                .filter(post -> post.getStatus().equals(PostStatus.PUBLISHED)).count();
+    }
+}
