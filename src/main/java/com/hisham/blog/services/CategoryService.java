d07634b585b84496b7eec230d224d47144d6601c
Hisham Mohammed <hishammohammed1199@gmail.com>
2025-12-11 10:31:57 +0300

    feat: implement deleteCategory endpoint service logic

--- src/main/java/com/hisham/blog/services/CategoryService.java
+++ src/main/java/com/hisham/blog/services/CategoryService.java
@@ -4,9 +4,11 @@ import com.hisham.blog.domain.dtos.CategoryDto;
 import com.hisham.blog.domain.entities.Category;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface CategoryService {
     List<Category> listCategories();
     Category createCategory(Category category);
-
+    void deleteCategory(UUID uuid);
 }
