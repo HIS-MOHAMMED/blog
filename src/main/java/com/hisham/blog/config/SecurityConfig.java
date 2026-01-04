74c07135315babea902603853b97faac5e89726d
Hisham Mohammed <hishammohammed1199@gmail.com>
2026-01-04 21:03:26 +0300

    feat: implement JWT authentiction with security chain integration (#15)
    * added jwt filter to intercept requests and validate Bearer tokens
    
    * modified auth service to include token validation logic and username extraction from claims
    
    * modified BlogUserDetails to expose the uuid of the user and attach it to the request attributes of the filter
    
    * integrated JWT authentication filter into security filter chain
    
    * defined authentication service bean as final to be resolve ingection by lombok's annontation @RequiredArgsConstructor

--- src/main/java/com/hisham/blog/config/SecurityConfig.java
+++ src/main/java/com/hisham/blog/config/SecurityConfig.java
@@ -1,5 +1,10 @@
 package com.hisham.blog.config;
 
+import com.hisham.blog.domain.entities.User;
+import com.hisham.blog.repositories.UserRepository;
+import com.hisham.blog.security.BlogUserDetailsService;
+import com.hisham.blog.security.JWTAuthenticationFilter;
+import com.hisham.blog.services.AuthenticationService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -8,24 +13,52 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
-
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity)throws Exception{
+    public JWTAuthenticationFilter jwtAuthenticationFilter(AuthenticationService authenticationService){
+        return new JWTAuthenticationFilter(authenticationService);
+    }
+    /**
+     * Configures user details service; persists default user if missing
+     */
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepository) {
+        String email = "user@test.com";
+        userRepository.findByEmail(email).orElseGet(() -> {
+            //temporary: Builds default user with encoded password
+            User newUser = User.builder()
+                    .name("Test User")
+                    .email(email)
+                    .password(passwordEncoder().encode("password"))
+                    .build();
+            return userRepository.save(newUser);
+        });
+        return new BlogUserDetailsService(userRepository);
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity,
+                                                   JWTAuthenticationFilter jwtAuthenticationFilter
+                                                    ) throws Exception{
+        // Configures stateless authentication and authorization
         httpSecurity
+                // Configures public access for specified endpoints
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/api/v1/auth/login").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/posts/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/categories/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/tags/**").permitAll()
                         .anyRequest().authenticated()
                 ).csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session ->
-                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthenticationFilter , UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
     }
 
--- src/main/java/com/hisham/blog/services/AuthenticationService.java
+++ src/main/java/com/hisham/blog/services/AuthenticationService.java
@@ -5,4 +5,5 @@ import org.springframework.security.core.userdetails.UserDetails;
 public interface AuthenticationService {
     public UserDetails authenticate(String email, String password);
     public String generateToken(UserDetails userDetails);
+    public UserDetails validateToken(String token);
 }
